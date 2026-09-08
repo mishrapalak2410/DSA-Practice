c13efabff5d0b27465ba0bdaf1dce70e29b27dfa
Palak Mishra <mishrapalak2410@gmail.com>
2026-09-08 23:11:03 +0530

    Time: 18 ms (25.27%), Space: 11.4 MB (64.85%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,39 +1,57 @@
 class Solution {
 public:
+    string minWindow(string s, string t) {
 
-    bool check(vector<int>&window,vector<int>&need){
-            for(int i=0;i<256;i++){
-                if(window[i]<need[i])
-                        return false;
-            }
-            return true;
+        unordered_map<char, int> need;
+        unordered_map<char, int> window;
+
+        // Frequency of characters required
+        for(char ch : t) {
+            need[ch]++;
         }
 
+        int low = 0;
+        int count = 0;
 
-    string minWindow(string s, string t) {
-        vector<int> need(256,0);
-        vector<int> window(256,0);
-        
-        for (int i=0;i<t.size();i++){
-             need[t[i]]++;
-        }    
-        
-        int low=0;
-        int minlen=INT_MAX;
-        int start=0;
+        int minlen = INT_MAX;
+        int start = 0;
 
-        for(int high=0;high<s.size();high++){
+        for(int high = 0; high < s.size(); high++) {
+
+            // Add character to window
             window[s[high]]++;
-            while(check(window,need)){
-                int currentlen=high-low+1;
-                if(minlen>currentlen){
-                    minlen=currentlen;
-                    start=low;
+
+            // Character contributes to required count
+            if(need.count(s[high]) &&
+               window[s[high]] <= need[s[high]]) {
+                count++;
+            }
+
+            // Current window is valid
+            while(count == t.size()) {
+
+                int currentlen = high - low + 1;
+
+                if(currentlen < minlen) {
+                    minlen = currentlen;
+                    start = low;
                 }
+
+                // Remove left character
                 window[s[low]]--;
+
+                if(need.count(s[low]) &&
+                   window[s[low]] < need[s[low]]) {
+                    count--;
+                }
+
                 low++;
             }
         }
-        return  minlen==INT_MAX? "":s.substr(start,minlen);
+
+        if(minlen == INT_MAX)
+            return "";
+
+        return s.substr(start, minlen);
     }
 };
